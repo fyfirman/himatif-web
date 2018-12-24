996c9a1f76f7ef3a9aa24b841367988756c6e65d
Firmansyah Yanuar <fyfirman@gmail.com>
2018-12-24 22:46:26 +0700

    Set Up Materialize

--- config/app.php
+++ config/app.php
@@ -175,6 +175,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Materialize
+         */
+        Skydiver\LaravelMaterializeCSS\MaterializeCSSServiceProvider::class,
+
     ],
 
     /*
@@ -209,6 +214,7 @@ return [
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
+        'MaterializeCSS' => Skydiver\LaravelMaterializeCSS\MaterializeCSS::class,
         'Notification' => Illuminate\Support\Facades\Notification::class,
         'Password' => Illuminate\Support\Facades\Password::class,
         'Queue' => Illuminate\Support\Facades\Queue::class,
---
+++ resources/views/helloworld.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {!! MaterializeCSS::include_full() !!}
+    <title>Test</title>
+</head>
+
+<body>
+    <div class="row">
+        <div class="col s12 m6">
+            <div class="card blue-grey darken-1">
+                <div class="card-content white-text">
+                    <span class="card-title">Hello World</span>
+                    <p>This is test page to prove MaterializeCSS has been installed successfully.</p>
+                </div>
+                <div class="card-action">
+                    <a href="#">Yes</a>
+                    <a href="#">Good</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>

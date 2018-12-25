81c069a630036e89e18795b8b168932d9bd83457
Firmansyah Yanuar <fyfirman@gmail.com>
2018-12-26 06:24:17 +0700

    Add some component

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,15 @@
+<div class="navbar-fixed">
+    <nav class="white">
+        <div class="nav-wrapper">
+            <a href="#" class="brand-logo grey-text text-darken-1">
+                <img src="{{ asset('img/himatif-logo-256x256.png') }}" alt="">
+                Himatif Web
+            </a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">JavaScript</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,5 @@
 */
 
 Route::get('/', function () {
-    return view('helloworld');
+    return view('app');
 });

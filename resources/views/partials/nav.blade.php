6cb42996eaad47623e3fd0c8c4b566ca1b84a123
Firmansyah Yanuar <fyfirman@gmail.com>
2018-12-27 04:09:45 +0700

    Add logout form

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -7,10 +7,16 @@
         <ul id="nav-mobile" class="right hide-on-med-and-down">
             <li><a href="#"><i class="material-icons grey-text text-lighten-1">apps</i></a></li>
             <li>
-                <a class="nav-button" onclick="showLogin()">
-                    <img class="user-thumb" src="{{ asset('img/user-profile-dummy.jpg') }}" style="vertical-align:middle"/>
-                    <span class="grey-text">{{ Session::get('username') }}</span>
-                </a>
+                {{-- Kondisi ketika sudah masuk --}}
+                @if(session('logged_in'))
+                    <a class="nav-button" onclick="showLogout()">
+                        <img class="user-thumb" src="{{ asset('img/user-profile-dummy.jpg') }}" style="vertical-align:middle"/>
+                    </a>
+                {{-- Kondisi ketika belum masuk --}}
+                @else
+                    <a class="deep-btn login-btn btn-small" onclick="showLogin()">Login</a>
+                @endif
+                
             </li>
             
         </ul>
@@ -27,8 +33,6 @@
                 <input name="username" id="a" type="text" class="validate">
                 <label for="a">Nomor Pokok Mahasiswa</label>
             </div>
-        {{-- </div>
-        <div class="row"> --}}
             <div class="input-field col m12 inline" >
                 <input name="password" id="password" type="password" class="validate">
                 <label for="password">Password</label>
@@ -36,7 +40,7 @@
         </div>
         <div class="row valign-wrapper">
             <div class="col m6">
-                <button class="btn waves-effect waves-light" type="submit">
+                <button class="btn-small login-btn deep-btn" type="submit">
                     Login
                 </button>
             </div>
@@ -47,43 +51,44 @@
     </form>
 </div>
 
-<div id="logout-form" class="login-form">
-    <a href="logout" class="btn waves-effect waves-light red right">Logout</a>
+<div id="logout-form" class="logout-form">
+    <div class="row">
+        <div class="col m5 text-center">
+            <img class="user-thumb" src="{{ asset('img/user-profile-dummy.jpg') }}" style="vertical-align:middle"/>
+        </div>
+        <div class="col m7">
+            <div class="info-wrapper">
+                <div class="row user-name">
+                    Firmansyah Yanuar
+                </div>
+                <div class="row">
+                    1408101X00XX
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <a href="logout" class="btn-small deep-btn logout-btn">Logout</a>
+        <a href="logout" class="btn-small deep-btn edit-btn">Edit Profile</a>
+    </div>
 </div>
 
 <script>
     function showLogin() {
-        var l = "{{ Session::get('logged_in')}}";
         var x = document.getElementById("login-form");
+        if (x.style.display == "none" || x.style.display == '') {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+
+    function showLogout(){
         var y = document.getElementById('logout-form');
-        if(l == 1){
-            if (y.style.display == "none" || y.style.display == '') {
-                y.style.display = "block";
-            } else {
-                y.style.display = "none";
-            }
-        }else{
-            if (x.style.display == "none" || x.style.display == '') {
-                x.style.display = "block";
-            } else {
-                x.style.display = "none";
-            }
+        if (y.style.display == "none" || y.style.display == '') {
+            y.style.display = "block";
+        } else {
+            y.style.display = "none";
         }
     }
 </script>
-{{-- Dropdown Content --}}
-{{-- <div id="login-form" class="login-form">
-    <form action="" autocomplete="off">
-        <div class="input-field col s12">
-            <input id="npm" type="text" class="validate">
-            <label for="npm">Nomor Pokok Mahasiswa</label>
-        </div>
-        <div class="input-field col s12">
-            <input id="password" type="password" class="validate">
-            <label for="password">Password</label>
-        </div>
-        <button class="btn waves-effect waves-light" type="submit" name="action">
-            Login
-        </button>
-    </form>
-</div> --}}

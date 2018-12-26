5bd4c8300ed3c8fe6b98efa012980315e3195720
Firmansyah Yanuar <fyfirman@gmail.com>
2018-12-26 14:12:40 +0700

    Add Materialize SASS & Navbar Component

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
-<!-- <html lang="{{ str_replace('_', '-', app()->getLocale()) }}"> -->
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {!! MaterializeCSS::include_full() !!}
-    
+    <link rel="stylesheet" type="text/css" href="{{ mix('/css/app.css') }}">
     <title>{{ config('app.name') }}</title>
 </head>
 
 <body>
     @include('partials.nav')
 
-    <footer class="row">
+    {{-- <footer class="row">
         @include('partials.footer')
-    </footer>
+    </footer> --}}
 </body>
 
 </html>
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,15 +1,16 @@
-<div class="navbar-fixed">
-    <nav class="white">
-        <div class="nav-wrapper">
-            <a href="#" class="brand-logo grey-text text-darken-1">
-                <img src="{{ asset('img/himatif-logo-256x256.png') }}" alt="">
-                Himatif Web
-            </a>
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a href="sass.html">Sass</a></li>
-                <li><a href="badges.html">Components</a></li>
-                <li><a href="collapsible.html">JavaScript</a></li>
-            </ul>
-        </div>
-    </nav>
-</div>
+<nav class="navbar-fixed white">
+    <div class="nav-wrapper">
+        <a href="#" class="brand-logo grey-text text-darken-1">
+            <img src="{{ asset('img/himatif-logo-256x256.png') }}" alt="" style="vertical-align:middle">
+            <span>Himatif Web</span>
+        </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="#"><i class="material-icons grey-text text-lighten-1">apps</i></a></li>
+            <li>
+                <a href="#" class="nav-button">
+                    <img class="user-thumb" src="{{ asset('img/user-profile-dummy.jpg') }}" style="vertical-align:middle"/>
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>

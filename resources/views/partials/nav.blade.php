87a194f8d3794558d0e4905a37af5cff9dbbf520
Firmansyah Yanuar <fyfirman@gmail.com>
2018-12-26 20:00:23 +0700

    Create login form on navbar

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -7,10 +7,69 @@
         <ul id="nav-mobile" class="right hide-on-med-and-down">
             <li><a href="#"><i class="material-icons grey-text text-lighten-1">apps</i></a></li>
             <li>
-                <a href="#" class="nav-button">
+                <a class="nav-button" onclick="showLogin()">
                     <img class="user-thumb" src="{{ asset('img/user-profile-dummy.jpg') }}" style="vertical-align:middle"/>
                 </a>
             </li>
+            
         </ul>
     </div>
 </nav>
+
+
+{{-- Dropdown Content --}}
+<div id="login-form" class="login-form">
+    <form action="" autocomplete="off">
+        <div class="row">
+            <div class="input-field col m12 inline" >
+                <input id="a" type="text" class="validate">
+                <label for="a">Nomor Pokok Mahasiswa</label>
+            </div>
+        {{-- </div>
+        <div class="row"> --}}
+            <div class="input-field col m12 inline" >
+                <input id="password" type="password" class="validate">
+                <label for="password">Password</label>
+            </div>
+        </div>
+        <div class="row valign-wrapper">
+            <div class="col m6">
+                <button class="btn waves-effect waves-light" type="submit" name="action">
+                    Login
+                </button>
+            </div>
+            <div class="col m6">
+                <a href="">Forgot Password</a>
+            </div>
+            
+
+        </div>
+    </form>
+</div>
+<script>
+    function showLogin() {
+        var x = document.getElementById("login-form");
+        console.log(x.style.display);
+        if (x.style.display == "none" || x.style.display == '') {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+</script>
+{{-- Dropdown Content --}}
+{{-- <div id="login-form" class="login-form">
+    <form action="" autocomplete="off">
+        <div class="input-field col s12">
+            <input id="npm" type="text" class="validate">
+            <label for="npm">Nomor Pokok Mahasiswa</label>
+        </div>
+        <div class="input-field col s12">
+            <input id="password" type="password" class="validate">
+            <label for="password">Password</label>
+        </div>
+        <button class="btn waves-effect waves-light" type="submit" name="action">
+            Login
+        </button>
+    </form>
+</div> --}}

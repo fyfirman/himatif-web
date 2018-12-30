68ab40bd688b22d43537c6f9eebab9c792dc9fd8
Firmansyah Yanuar <fyfirman@gmail.com>
2018-12-30 23:04:02 +0700

    Create card

--- resources/views/hda/content.blade.php
+++ resources/views/hda/content.blade.php
@@ -4,17 +4,20 @@
     <div class="row">
         @foreach ($data as $item)
             @if ($item != NULL)
-                <div class="col l2 m3 s4">
+                <div class="col m3 s4 custom-col">
                     <div class="card">
-                        <div class="card-image">
-                            <img src="{{ $item->url_foto }}">
-                            <span class="card-title">{{ $item->nama }}</span>
-                            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">edit</i></a>
+                        <div class="card-image waves-effect waves-block waves-light">
+                            <img class="activator" src="{{ $item->url_foto }}">
                         </div>
-                        <div class="card-content center">
-                            <p>{{ $item->npm }}</p>
+                        <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4">{{ $item->nama }}<i class="material-icons right">more_vert</i></span>
+                            <p><a href="#"><p>{{ $item->npm }}</p>
                             <p>@php echo (isset($item->jabatan) ? $item->jabatan : ''); @endphp</p>
-                            <p>@php echo (isset($item->posisi) ? $item->posisi : ''); @endphp</p>
+                            <p>@php echo (isset($item->posisi) ? $item->posisi : ''); @endphp</p></a></p>
+                        </div>
+                        <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                            <p>Here is some more information about this product that is only revealed once clicked on.</p>
                         </div>
                     </div>
                 </div>

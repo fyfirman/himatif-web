591b0cb00d2a322b1409e82c2ec7276fdee4d8ee
Firmansyah Yanuar <fyfirman@gmail.com>
2018-12-30 18:01:44 +0700

    Refactoring code using ternary operator

--- resources/views/hda/content.blade.php
+++ resources/views/hda/content.blade.php
@@ -4,22 +4,22 @@
     <div class="row">
         @foreach ($data as $item)
             @if ($item != NULL)
-                @if (isset($item->posisi) && isset($item->jabatan))
-                    <div class="col l2 m3 s4">
-                        <div class="card">
-                            <div class="card-image">
-                                <img src="{{ $item->url_foto }}">
-                                <span class="card-title">{{ $item->nama }}</span>
-                                <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">edit</i></a>
-                            </div>
-                            <div class="card-content center">
-                                <p>{{ $item->npm }}</p>
-                                <p>{{ $item->jabatan }}</p>
-                                <p>{{ $item->posisi }}</p>
-                            </div>
+                {{-- @if (isset($item->posisi) && isset($item->jabatan)) --}}
+                <div class="col l2 m3 s4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="{{ $item->url_foto }}">
+                            <span class="card-title">{{ $item->nama }}</span>
+                            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">edit</i></a>
+                        </div>
+                        <div class="card-content center">
+                            <p>{{ $item->npm }}</p>
+                            <p>@php echo (isset($item->jabatan) ? $item->jabatan : ''); @endphp</p>
+                            <p>@php echo (isset($item->posisi) ? $item->posisi : ''); @endphp</p>
                         </div>
                     </div>
-                @elseif(isset($item->jabatan))
+                </div>
+                {{-- @elseif(isset($item->jabatan))
                     <div class="col l2 m3 s4">
                         <div class="card">
                             <div class="card-image">
@@ -46,7 +46,7 @@
                             </div>
                         </div>
                     </div>
-                @endif
+                @endif --}}
             @endif
         @endforeach
     </div>

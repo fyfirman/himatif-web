670e6975d2279717139297811ce4c5f8a3b02043
Firmansyah Yanuar <fyfirman@gmail.com>
2018-12-31 02:05:48 +0700

    Fix active sidenav

--- resources/views/hda/sidenav.blade.php
+++ resources/views/hda/sidenav.blade.php
@@ -1,5 +1,5 @@
 <ul id="slide-out" class="sidenav sidenav-fixed">
-    <li><a href="#!" class="waves-effect">Tentang</a></li>
+    <li><a id="tentang" href="#!" class="waves-effect">Tentang</a></li>
     <li class="no-padding">
         <ul class="collapsible collapsible-accordion">
             <li class="active">
@@ -11,14 +11,14 @@
                         <li><a href="{{ url('hda/angkatan/2014') }}" class="waves-effect">Assembly 2014</a></li>
                         <li><a href="{{ url('hda/angkatan/2015') }}" class="waves-effect">Binary 2015</a></li>
                         <li><a href="{{ url('hda/angkatan/2016') }}" class="waves-effect">Cyber 2016</a></li>
-                        <li class="active"><a href="{{ url('hda/angkatan/2017') }}" class="waves-effect">Delphi 2017</a></li>
+                        <li><a href="{{ url('hda/angkatan/2017') }}" class="waves-effect">Delphi 2017</a></li>
                         <li><a href="{{ url('hda/angkatan/2018') }}" class="waves-effect">Eclipse 2018</a></li>
                     </ul>
                 </div>
             </li> 
         </ul>
         <ul class="collapsible collapsible-accordion">
-            <li>
+            <li class="active">
                 <a class="collapsible-header">Badan Kelengkapan</a>
                 <div class="collapsible-body">
                     <ul>
@@ -34,9 +34,26 @@
 </ul>
 
 <script>
-    // Or with jQuery
+//Collapsible
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+});
 
-    $(document).ready(function(){
-        $('.collapsible').collapsible();
-    });
+//
+$(function() {
+    var pgurl = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
+    
+    //collapsible menu
+    $(".collapsible-body li a").each(function(){
+        let ahref = $(this).attr("href").split('/').pop();
+        if(ahref == pgurl)
+            $(this).parent().addClass("active");
+    })
+
+    $(".sidenav > li > a").each(function(){
+        let id = $(this).attr("id");
+        if(id == pgurl || pgurl == '')
+            $(this).parent().addClass("active");
+    })
+});
 </script>

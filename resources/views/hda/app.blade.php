245c44e8b28edda68ed5348a694c10f1b569a1c8
Firmansyah Yanuar <fyfirman@gmail.com>
2019-01-05 09:01:39 +0700

    Add detail profile view

--- resources/views/hda/app.blade.php
+++ resources/views/hda/app.blade.php
@@ -13,6 +13,7 @@
 </head>
 
 <body>
+    @include('hda.detailProfile')
     @include('partials.nav-hda')
     @include('hda.sidenav')
     <main class="section">
@@ -21,49 +22,49 @@
             {{-- Ajax Request --}}
         </div>
         {{-- Loading anim --}}
-            <div id="loading" class="center hide">
-                <div class="preloader-wrapper big active">
-                    <div class="spinner-layer spinner-blue">
-                        <div class="circle-clipper left">
-                        <div class="circle"></div>
-                        </div><div class="gap-patch">
-                        <div class="circle"></div>
-                        </div><div class="circle-clipper right">
-                        <div class="circle"></div>
-                        </div>
+        <div id="loading" class="center hide">
+            <div class="preloader-wrapper big active">
+                <div class="spinner-layer spinner-blue">
+                    <div class="circle-clipper left">
+                    <div class="circle"></div>
+                    </div><div class="gap-patch">
+                    <div class="circle"></div>
+                    </div><div class="circle-clipper right">
+                    <div class="circle"></div>
                     </div>
+                </div>
 
-                    <div class="spinner-layer spinner-red">
-                        <div class="circle-clipper left">
-                        <div class="circle"></div>
-                        </div><div class="gap-patch">
-                        <div class="circle"></div>
-                        </div><div class="circle-clipper right">
-                        <div class="circle"></div>
-                        </div>
+                <div class="spinner-layer spinner-red">
+                    <div class="circle-clipper left">
+                    <div class="circle"></div>
+                    </div><div class="gap-patch">
+                    <div class="circle"></div>
+                    </div><div class="circle-clipper right">
+                    <div class="circle"></div>
                     </div>
+                </div>
 
-                    <div class="spinner-layer spinner-yellow">
-                        <div class="circle-clipper left">
-                        <div class="circle"></div>
-                        </div><div class="gap-patch">
-                        <div class="circle"></div>
-                        </div><div class="circle-clipper right">
-                        <div class="circle"></div>
-                        </div>
+                <div class="spinner-layer spinner-yellow">
+                    <div class="circle-clipper left">
+                    <div class="circle"></div>
+                    </div><div class="gap-patch">
+                    <div class="circle"></div>
+                    </div><div class="circle-clipper right">
+                    <div class="circle"></div>
                     </div>
+                </div>
 
-                    <div class="spinner-layer spinner-green">
-                        <div class="circle-clipper left">
-                        <div class="circle"></div>
-                        </div><div class="gap-patch">
-                        <div class="circle"></div>
-                        </div><div class="circle-clipper right">
-                        <div class="circle"></div>
-                        </div>
+                <div class="spinner-layer spinner-green">
+                    <div class="circle-clipper left">
+                    <div class="circle"></div>
+                    </div><div class="gap-patch">
+                    <div class="circle"></div>
+                    </div><div class="circle-clipper right">
+                    <div class="circle"></div>
                     </div>
                 </div>
             </div>
+        </div>
     </main>
 
     <footer class="row">
---
+++ resources/views/hda/detailProfile.blade.php
@@ -0,0 +1,61 @@
+<div id="overlay">
+    <div id="detail-container" class="container">
+        <div id="header-detail">
+            <img id="user-photos" class="user-thumb" src="http://localhost/REST-API-himatif/assets/foto/2017/41.jpg" alt="">
+            <h5>Fahmi Auliya</h5>
+            <p>140810170041 - Delphi 2017</p>
+            <p>Sistem Informasi</p>
+            <div class="contact row">
+                <div class="col m4">
+                    <i class="material-icons">call</i> 089291733121
+                </div>
+                <div class="col m4">
+                    <i class="fab fa-line"></i> fahmiau
+                </div>
+                <div class="col m4">
+                    <i class="material-icons">email</i> fahmiauau@gmail.com
+                </div>
+            </div>
+        </div>
+        <div id="body-detail">
+            <h4>Biodata</h4>
+            <div id="biodata">
+                <div class="row">
+                    <div class="col m12">
+                        <label for="jk">Jenis Kelamin</label>
+                        <p>Laki-Laki</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col m6">
+                        <label for="tmp_lahir">Tempat Lahir</label>
+                        <p>Bandung</p>
+                    </div>
+                    <div class="col m6">
+                        <label for="tgl_lahir">Tanggal Lahir</label>
+                        <p>23 Januari 1999</p>
+                    </div>
+                </div>
+                <div class="row">
+                        <div class="col m12">
+                            <label for="agama">Agama</label>
+                            <p>Islam</p>
+                        </div>
+                    </div>
+                <div class="row">
+                    <div class="col m12">
+                        <label for="alamat_rumah">Alamat Rumah</label>
+                        <p>Ujung berungsdadisa dsadhbiashu bduiashdsia u h</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col m12">
+                        <label for="alamat_kos">Alamat Kos</label>
+                        <p>Sukawening</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>

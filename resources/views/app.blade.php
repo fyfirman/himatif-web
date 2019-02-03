b55988eddf8b43718562e4c34884c7d45a87fbc4
Firmansyah Yanuar <fyfirman@gmail.com>
2019-02-03 16:56:54 +0700

    Edit landing page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {!! MaterializeCSS::include_full() !!}
+    
+    @yield('css')
     <link rel="stylesheet" type="text/css" href="{{ mix('/css/app.css') }}">
+    
     
     <title>{{ config('app.name') }}</title>
 </head>
--- resources/views/content/landing.blade.php
+++ resources/views/content/landing.blade.php
@@ -1,22 +1,51 @@
 @extends('app')
 
+@section('css')
+	<link rel="stylesheet" type="text/css" href="{{ mix('/css/landing.css') }}">
+@endsection
+
 @section('content')
 <div class="landing-container">
 	<div class="row">
-		@for ($i = 0; $i < 3; $i++)
-			<div class="col m4">
-				<div class="card">
-					<div class="card-image">
-						<img src="{{asset('img/hda-card.jpg')}}">
-						<a href="{{ url('/hda') }}" class="btn-floating btn-large halfway-fab waves-effect waves-light red"><i class="material-icons">send</i></a>
-					</div>
-					<div class="card-content">
-						<span class="card-title">Himatif Database</span>
-						<p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-					</div>
+		{{-- HDA Card --}}
+		<div class="col m4">
+			<div id="hda" class="card">
+				<div class="card-image">
+					<img src="{{asset('img/hda-card.jpg')}}">
+					<a href="{{ url('/hda') }}" class="btn-floating btn-large halfway-fab waves-effect waves-light red"><i class="material-icons">send</i></a>
+				</div>
+				<div class="card-content">
+					<span class="card-title">Himatif Database</span>
+					<p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
 				</div>
 			</div>
-		@endfor
+		</div>
+		{{-- Pathways Card --}}
+		<div class="col m4">
+			<div id="pathways" class="card">
+				<div class="card-image">
+					<img src="{{asset('img/hda-card.jpg')}}">
+					<a href="{{ url('/hda') }}" class="btn-floating btn-large halfway-fab waves-effect waves-light red"><i class="material-icons">send</i></a>
+				</div>
+				<div class="card-content">
+					<span class="card-title">Pathways</span>
+					<p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+				</div>
+			</div>
+		</div>
+		{{-- Read D'Journal Card --}}
+		<div class="col m4">
+			<div id="journal" class="card">
+				<div class="card-image">
+					<img src="{{asset('img/hda-card.jpg')}}">
+					<a href="{{ url('/hda') }}" class="btn-floating btn-large halfway-fab waves-effect waves-light red"><i class="material-icons">send</i></a>
+				</div>
+				<div class="card-content">
+					<span class="card-title">Read D'Journal</span>
+					<p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
 @endsection

45167c097c558a65fd93a53503d378158c514b5e
Firmansyah Yanuar <fyfirman@gmail.com>
2019-02-03 18:39:47 +0700

    Change search placeholder

--- resources/views/partials/nav-hda.blade.php
+++ resources/views/partials/nav-hda.blade.php
@@ -4,7 +4,7 @@
     <div class="search-wrapper left hide-on-med-and-down">
         <form class="hide-on-med-and-down" id="search-form" autocomplete="off">
             <div class="input-field">
-                <input id="search-box" type="search" required onkeyup="" placeholder="Cari dengan NPM">
+                <input id="search-box" type="search" required onkeyup="" placeholder="Cari dengan nama atau NPM">
                 <label class="label-icon" for="search"><i class="material-icons">search</i></label>
             </div>
         </form> 

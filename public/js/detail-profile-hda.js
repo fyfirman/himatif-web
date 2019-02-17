744fd303c9645b373682755de0568fbdc8565495
atjhoendz <achunarmando@gmail.com>
2019-02-17 23:55:34 +0700

    Add detail profile for admin

--- public/js/detail-profile-hda.js
+++ public/js/detail-profile-hda.js
@@ -19,11 +19,18 @@ function showInfo(npm){
                 $('#line').text(value.line);
                 $('#email').text(value.email);
                 $('#jk').text(value.jk);
+                $('#hobi').text(value.hobi);
                 $('#tempat_lahir').text(value.tempat_lahir);
                 $('#tanggal_lahir').text(value.tanggal_lahir);
                 $('#agama').text(value.agama);
                 $('#alamat_rumah').text(value.alamat_rumah);
                 $('#alamat_kos').text(value.alamat_kos);
+                $('#nama_ayah').text(value.nama_ayah);
+                $('#nama_ibu').text(value.nama_ibu);
+                $('#no_telp_ortu').text(value.no_tlp_ortu);
+                $('#facebook').text(value.fb);
+                $('#twitter').text(value.twitter);
+                $('#instagram').text(value.instagram);
             });
         }
     });

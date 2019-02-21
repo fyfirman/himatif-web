60c780797658d89e103283a4bc0bb5bca22d6809
Firmansyah Yanuar <fyfirman@gmail.com>
2019-02-22 00:15:04 +0700

    Add card for pathways

--- resources/views/keilmuan/pathways.blade.php
+++ resources/views/keilmuan/pathways.blade.php
@@ -1,18 +1,22 @@
 @extends('keilmuan.app')
 
 @section('content')
+    @for ($i = 0; $i < 4; $i++)
     <div class="row">
+        @for ($j = 0; $j < 4; $j++)
         <div class="col s12 m3">
             <div class="card">
-            <div class="card-image">
-                <img src="https://materializecss.com/images/sample-1.jpg">
-                {{-- <span class="card-title">Card Title</span> --}}
-                <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">file_download</i></a>
-            </div>
-            <div class="card-content">
-                <p>Pathways Semester 2</p>
-            </div>
+                <div class="card-image">
+                    <img src="https://materializecss.com/images/sample-1.jpg">
+                    {{-- <span class="card-title">Card Title</span> --}}
+                    <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">file_download</i></a>
+                </div>
+                <div class="card-content">
+                    <p>Pathways Semester 2</p>
+                </div>
             </div>
         </div>
+        @endfor
     </div>
+    @endfor
 @endsection

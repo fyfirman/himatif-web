6107f393239d121febdbfd2cb0cac0b27fe90e87
Firmansyah Yanuar <fyfirman@gmail.com>
2019-03-23 18:00:16 +0700

    Add pathways thumbnail

--- resources/views/keilmuan/pathways.blade.php
+++ resources/views/keilmuan/pathways.blade.php
@@ -6,7 +6,7 @@
             <div class="col s12 m3">
                 <div class="card">
                     <div class="card-image">
-                        <img src="https://materializecss.com/images/sample-1.jpg">
+                    <img src="{{asset('svg/pathways-thumbnail.svg')}}">
                     <a href="{{ url('keilmuan/addCounter/'.$item->filename) }}" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">file_download</i></a>
                     </div>
                     <div class="card-content">
